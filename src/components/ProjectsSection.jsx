65938de169735931133ac25faf7fcaeab24e13ef
Abhinandg2003 <abhinandg2003@gmail.com>
2025-11-25 13:08:54 +0530

    space bw featured projects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -34,10 +34,9 @@ const projects = [
 export const ProjectsSection = () => {
     return <section id="projects" className="py-24 px-4 relative">
         <div className="container mx-auto max-w-5xl">
-            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Featured
-                <span className="text-primary">
-                    Projects
-                </span>
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Featured <span className="text-primary" >
+                Projects
+            </span>
             </h2>
 
             <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
@@ -67,12 +66,12 @@ export const ProjectsSection = () => {
                             <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
                             <div className="flex justify-between items-center">
                                 <div className="flex space-x-3">
-                                    {project.id !== 3 &&(
-                                    <a href={project.demoUrl}
-                                        target="_blank"
-                                        className="text-foreground/80 hover:text-primary transition=colors duration-300">
-                                        <ExternalLink size={20} />
-                                    </a>
+                                    {project.id !== 3 && (
+                                        <a href={project.demoUrl}
+                                            target="_blank"
+                                            className="text-foreground/80 hover:text-primary transition=colors duration-300">
+                                            <ExternalLink size={20} />
+                                        </a>
                                     )}
                                     <a href={project.githubUrl}
                                         target="_blank"
@@ -91,10 +90,10 @@ export const ProjectsSection = () => {
 
             </div>
             <div className="text-center mt-12">
-                <a href="https://github.com/Abhinandg2003" 
-                target = "_blank" 
-                className="cosmic-button w-fit flex items-center mx-auto gap-2">
-                    My Github Profile <ArrowRight size={16}/>
+                <a href="https://github.com/Abhinandg2003"
+                    target="_blank"
+                    className="cosmic-button w-fit flex items-center mx-auto gap-2">
+                    My Github Profile <ArrowRight size={16} />
 
                 </a>
             </div>
