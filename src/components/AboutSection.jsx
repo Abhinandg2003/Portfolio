82f3a90c13e02fe4681a4cf28f13a0dfac106339
Abhinandg2003 <abhinandg2003@gmail.com>
2026-02-16 12:16:18 +0530

    cv link6

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -25,7 +25,7 @@ export const AboutSection =() =>{
                             Contact Me
                         </a>
 
-                        <a href="https://drive.google.com/file/d/1uTccbTmvl7kCsTk-h4hZ1QQ8DZBEZ8Fr/view?usp=sharing" target="_blank" className="cosmic-button">
+                        <a href="https://drive.google.com/file/d/1fjfQuo5dUw374TogkK7gLusrTe50dk-z/view?usp=sharing" target="_blank" className="cosmic-button">
                             Download CV
                         </a>
 
