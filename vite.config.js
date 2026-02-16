d9d52e640f0c2ffa3f27ee8f9fe1fd888f772ff6
Abhinandg2003 <abhinandg2003@gmail.com>
2026-02-16 21:35:07 +0530

    firebase

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VERCEL ? "/" : "/Portfolio/",
-  plugins: [react(),tailwindcss()],
-  resolve:{
-    alias:{
+export default defineConfig(({ mode }) => ({
+  base: mode === 'gh' ? '/Portfolio/' : '/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
       "@": path.resolve(__dirname, "./src"),
-       },
+    },
   },
-});
-
-
+}));
