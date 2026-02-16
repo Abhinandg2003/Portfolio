aa40c4a57e838d42aa3aa67a7d658fe6595bb7fa
Abhinandg2003 <abhinandg2003@gmail.com>
2026-02-16 14:09:45 +0530

    vercel fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+  base: process.env.VERCEL ? "/" : "/Portfolio/",
   plugins: [react(),tailwindcss()],
   resolve:{
     alias:{
